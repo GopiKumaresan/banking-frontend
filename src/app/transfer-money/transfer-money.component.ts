16a8503b73dff791263a552dacee156daff02b3d
GopiKumaresan <gopikumaresan97@gmail.com>
2024-06-25 09:05:02 +0530

    Initial commit

---
+++ src/app/transfer-money/transfer-money.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transfer-money',
+  standalone: true,
+  imports: [],
+  templateUrl: './transfer-money.component.html',
+  styleUrl: './transfer-money.component.scss'
+})
+export class TransferMoneyComponent {
+
+}
